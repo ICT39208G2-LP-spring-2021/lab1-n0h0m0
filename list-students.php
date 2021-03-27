99a8761b08873e28235b249d022f8d0a96b22026
n0h0m0 <aleksandregasparyan@gmail.com>
2021-03-27 16:09:33 +0400

    პირველი ლაბორატორიული სამუშაო

---
+++ list-students.php
@@ -0,0 +1,26 @@
+
+
+<?php
+
+$conn = new mysqli('localhost','root','','gtu_lms');
+
+if(!$conn){
+    die("კავშირი ბაზასთან ვერ დამყარდა".$conn->connect_error);
+}
+$sql = 'SELECT FirstName, LastName, PersonalNumber FROM students';
+$value = mysqli_query($conn , $sql);
+
+if(!$value){
+    echo "მონაცემები ვერ მოიძებნა";
+}
+
+echo "<table>";
+while($row = mysqli_fetch_assoc($value)) {
+    echo "First name : {$row['FirstName']} <br> ";
+    echo "Last name : {$row['LastName']} <br> ";
+    echo "Personal Number : {$row['PersonalNumber']} <br> ";
+    echo "<hr>";
+}
+echo "</table>";
+
+?>
