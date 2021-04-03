16217648b41f92f6822296b70d2f078c4da24275
n0h0m0 <aleksandregasparyan@gmail.com>
2021-04-03 11:14:15 +0400

    პირველი ლაბი

--- 3/list-students.php
+++ 3/list-students.php
@@ -1,21 +1,20 @@
-
-
 <?php
 
-$conn = new mysqli('localhost','root','','gtu_lms');
+$conn = new mysqli('localhost', 'root', '', 'gtu_lms');
 
-if(!$conn){
-    die("კავშირი ბაზასთან ვერ დამყარდა".$conn->connect_error);
+if (!$conn) {
+    die("კავშირი ბაზასთან ვერ დამყარდა" . $conn->connect_error);
 }
-$sql = 'SELECT FirstName, LastName, PersonalNumber FROM students';
-$value = mysqli_query($conn , $sql);
 
-if(!$value){
+$sql = 'SELECT FirstName, LastName, PersonalNumber FROM students';
+$value = mysqli_query($conn, $sql);
+
+if (!$value) {
     echo "მონაცემები ვერ მოიძებნა";
 }
 //გამოგვაქვს სტუდენტის სახელი გვარი და პერსონალური ნომერი.
-echo "<table>";
-while($row = mysqli_fetch_assoc($value)) {
+echo "<table style='width: 50%; margin: auto;padding: 0px'>";
+while ($row = mysqli_fetch_assoc($value)) {
     echo "First name : {$row['FirstName']} <br> ";
     echo "Last name : {$row['LastName']} <br> ";
     echo "Personal Number : {$row['PersonalNumber']} <br> ";
